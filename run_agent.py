0422255c57b301f125af36d4ce088e5f1d372309
Tristan Heywood <tristanheywood@hotmail.com>
2022-08-13 21:25:52 +1000

    add option to save run_agent video

--- run_agent.py
+++ run_agent.py
@@ -3,10 +3,11 @@ import pickle
 
 import aicrowd_gym
 import minerl
+import cv2
 
 from openai_vpt.agent import MineRLAgent
 
-def main(model, weights, env, n_episodes=3, max_steps=int(1e9), show=False):
+def main(model, weights, env, n_episodes=3, max_steps=3600, show=False, save=False):
     # Using aicrowd_gym is important! Your submission will not work otherwise
     env = aicrowd_gym.make(env)
     agent_parameters = pickle.load(open(model, "rb"))
@@ -15,6 +16,12 @@ def main(model, weights, env, n_episodes=3, max_steps=int(1e9), show=False):
     pi_head_kwargs["temperature"] = float(pi_head_kwargs["temperature"])
     agent = MineRLAgent(env, policy_kwargs=policy_kwargs, pi_head_kwargs=pi_head_kwargs)
     agent.load_weights(weights)
+
+    VIDEO_FPS = 20
+    VIDEO_SHAPE = (400, 300)
+    video_writer = cv2.VideoWriter(
+        "agent3.mp4", cv2.VideoWriter_fourcc(*"mp4v"), VIDEO_FPS, VIDEO_SHAPE
+    )
 
     for _ in range(n_episodes):
         obs = env.reset()
@@ -27,9 +34,20 @@ def main(model, weights, env, n_episodes=3, max_steps=int(1e9), show=False):
             obs, _, done, _ = env.step(action)
             if show:
                 env.render()
+            if save:
+                frame = env.render("rgb_array")
+
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                frame = cv2.resize(frame, VIDEO_SHAPE)
+
+                video_writer.write(frame)
+
             if done:
                 break
     env.close()
+
+    video_writer.release()
+    del video_writer
 
 
 if __name__ == "__main__":
@@ -39,7 +57,8 @@ if __name__ == "__main__":
     parser.add_argument("--model", type=str, required=True, help="Path to the '.model' file to be loaded.")
     parser.add_argument("--env", type=str, required=True)
     parser.add_argument("--show", action="store_true", help="Render the environment.")
+    parser.add_argument("--save", action="store_true", help="Save the frames as a video.")
 
     args = parser.parse_args()
 
-    main(args.model, args.weights, args.env, show=args.show)
+    main(args.model, args.weights, args.env, show=args.show, save=args.save)
